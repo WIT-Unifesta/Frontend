334a8c41c84d743b177e159f650daf9cc641d6e5
serin301 <63085375+serin301@users.noreply.github.com>
2022-09-15 14:37:29 +0900

    ADD: TypeScript 검출을 위한 라이브러리 추가

--- unifesta/App.tsx
+++ unifesta/App.tsx
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Cr>aa</Cr>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const Cr = styled.Text`
+  color: red;
+`
 
 const styles = StyleSheet.create({
   container: {
