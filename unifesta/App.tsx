5443871e4cc15650d71fd662e3f2ae264051caee
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-09-26 10:58:09 +0900

    ADD: 홈화면>학교상세>축제소개
    기본 배치

--- unifesta/App.tsx
+++ unifesta/App.tsx
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import styled from 'styled-components/native'
+import { createStackNavigator } from '@react-navigation/stack'; 
+import SchoolDetail from './SchoolDetail';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator(); 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Cr>aa</Cr>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="SchoolDetail" component={SchoolDetail} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
@@ -19,8 +25,17 @@ const Cr = styled.Text`
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    paddingHorizontal:20,
+  },
+  header:{
+    justifyContent:"space-between",
+    flexDirection:"row",
+    marginTop:100,
+  },
+  btnText:{
+    fontSize:44,
+    fontWeight:"600",
+    color:"white",
   },
 });
---
+++ unifesta/SchoolDetail.tsx
@@ -0,0 +1,126 @@
+import { StyleSheet, Text, View, Button,Image, LayoutAnimation,useWindowDimensions  } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import {StatusBar} from 'react-native';
+import styled from 'styled-components/native'
+import {TabView,SceneMap} from 'react-native-tab-view';
+import {useState,useEffect} from 'react';
+
+const HeaderContainer = styled.View`
+    flex:1;
+    margin-top: 10px;
+    flex-direction: column;
+    background-color: gray;
+`;
+const HeaderBar = styled.View`
+    flex:1;
+    flex-direction: row;
+`;
+const HeaderSchoolName = styled.View`
+    flex:1;
+    flex-direction: row;
+`;
+const MainContainer = styled.View`
+    flex:3;
+`;
+const TabBar = styled.View`
+    flex-direction:row;
+`;
+const FestivalInformation = () => (
+    <View style={{ flex: 1, padding:10, }}>
+       <View style={{flex:1,flexDirection:'row',borderColor:'gray',}}>
+        <View style={{margin:10,}}>
+            <Text style={{fontSize:15,paddingBottom:2,}}>낮 프로그램</Text>
+            <Text style={{fontSize:15,paddingBottom:2,}}>저녁 주점</Text>
+            <Text style={{fontSize:15,paddingBottom:2,}}>공연</Text>
+            <Text style={{fontSize:15,paddingBottom:2,}}>주소</Text>
+        </View>
+        <View style={{margin:10,}}>
+            <Text style={{fontSize:15,paddingBottom:2,}}>10:00 ~ 18:00</Text>
+            <Text style={{fontSize:15,paddingBottom:2,}}>18:00 ~ 22:00</Text>
+            <Text style={{fontSize:15,paddingBottom:2,}}>18:00 ~ 22:00</Text>
+            <Text style={{fontSize:15,paddingBottom:2,}}>서울시 광진구 능동로 120</Text>
+        </View>
+       </View>
+       <View style={{flex:1,alignItems:'center'}}>
+        <Image source={require('./assets/festivalposter.jpg')} style={{width:300,height:100,}}/>
+       </View>
+       <View style={{flex:1,}}>
+        <Text style={{fontSize:18,fontWeight:'bold',}}>축제 소개</Text>
+        <Text>10월 말경에는 5월 일감호 냄새 너무나요 김수한무 거북이와 두루미 삼천갑자 동방삭 치치카포 사리사리센타 워리워리 세브리깡 무두셀라 구름이 허리케인에 담벼락 담벼락에 서생원 서생원에 고양이 고양이엔 바둑이 바둑이는 돌돌이</Text>
+       </View>
+       <View style={{flex:1,}}>
+        <Text style={{fontSize:18,fontWeight:'bold',}}>라인업</Text>
+       </View>
+    </View>
+  );
+  
+const FestivalLocation = () => (
+    <View style={{ flex: 1, }}>
+        <Text  style={{fontSize:100}}>배치도</Text>
+    </View>
+);
+const FestivalReview = () => (
+    <View style={{ flex: 1, }}>
+        <Text style={{fontSize:100}}>
+            축제 후기
+        </Text>
+    </View>
+);
+const RestaurantAround = () => (
+    <View style={{ flex: 1, }}>
+        <Text style={{fontSize:100}}>
+            주변 맛집
+        </Text>
+    </View>
+);
+
+
+const renderScene = SceneMap({
+    first: FestivalInformation,
+    second: FestivalLocation,
+    third:FestivalReview,
+    fourth:RestaurantAround,
+});
+
+const SchoolDetail = () => { 
+    const topmargin = StatusBar.currentHeight;
+    StatusBar.setBarStyle('dark-content');
+    const layout = useWindowDimensions();
+    const [index, setIndex] = useState(0);
+    const [routes] = useState([
+        { key: 'first', title: '축제소개' },
+        { key: 'second', title: '배치도' },
+        { key: 'third', title:'축제 후기'},
+        { key: 'fourth', title:'주변 맛집'}
+    ]);
+	return (
+        <View style={{marginTop:topmargin,flex:1,paddingTop:25,}}>
+            <HeaderContainer>
+                <HeaderBar>
+                    <View style={{flex:1,padding:8,}}><Icon name="left" style={{fontSize:25,color:'white'}}/></View>
+                    <View style={{flex:8,}}></View>
+                    <View style={{flex:1,padding:8,}}><Icon name="sharealt" style={{fontSize:25,color:'white'}}/></View>
+                    <View style={{flex:1,padding:8,marginRight:10,}}><Icon name="hearto" style={{fontSize:25,color:'white'}}/></View>
+                </HeaderBar>
+                <View style={{flex:1,}}></View>
+                <HeaderSchoolName>
+                    <Image source={require('./assets/konkuklogo.png')} style={{height:60,width:60,marginLeft:10,padding:1,}} />
+                    <View style={{marginLeft:10,}}>
+                        <Text style={{fontSize:12,color:'white',padding:1,}}>D-10</Text>
+                        <Text style={{fontSize:20,color:'white',padding:1,}}>건국대학교 서울캠퍼스</Text>
+                        <Text style={{fontSize:12,color:'white',padding:1,}}>2022 / 09.28 ~ 09.30</Text>
+                    </View>
+                </HeaderSchoolName>
+            </HeaderContainer>
+            <MainContainer>
+                <TabView 
+                    navigationState={{index,routes}} 
+                    renderScene={renderScene}
+                    onIndexChange={setIndex} 
+                    initialLayout={{width:layout.width}}
+                />
+            </MainContainer>
+        </View>
+    ) 
+} 
+export default SchoolDetail;
