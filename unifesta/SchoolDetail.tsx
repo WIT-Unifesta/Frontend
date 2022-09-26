c2f260184ea960425e328f41db49abbf03218d92
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-09-26 11:13:22 +0900

    ADD: 홈화면 > 학교상세 > 축제후기
    기본 배치

--- unifesta/SchoolDetail.tsx
+++ unifesta/SchoolDetail.tsx
@@ -92,9 +92,32 @@ const FestivalLocation = () => (
 );
 const FestivalReview = () => (
     <View style={{ flex: 1, }}>
-        <Text style={{fontSize:100}}>
-            축제 후기
-        </Text>
+        <View style={{flex:2,margin:10,}}>
+            <Button title="후기 쓰기" />
+        </View>
+        <View style={{flex:1,flexDirection:'row',}}>
+            <View style={{borderColor:'gray',borderWidth:1,margin:10,borderRadius:10,padding:1,}}>
+                <Text>2019</Text>
+            </View>
+            <View style={{borderColor:'gray',borderWidth:1,margin:10,borderRadius:10,padding:1,}}>
+                <Text>2020</Text>
+            </View>
+            <View style={{borderColor:'gray',borderWidth:1,margin:10,borderRadius:10,padding:1,}}>
+                <Text>2021</Text>
+            </View>
+        </View>
+        <View style={{flex:8,}}>
+            <Text>학생 1</Text>
+            <Text>레드벨벳 가웅</Text>
+            <Text>학생 1</Text>
+            <Text>레드벨벳 가웅</Text>
+            <Text>학생 1</Text>
+            <Text>레드벨벳 가웅</Text>
+            <Text>학생 1</Text>
+            <Text>레드벨벳 가웅</Text>
+            <Text>학생 1</Text>
+            <Text>레드벨벳 가웅</Text>
+        </View>
     </View>
 );
 const RestaurantAround = () => (
