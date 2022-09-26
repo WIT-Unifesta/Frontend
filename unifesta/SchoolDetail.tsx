2c0f098533667b738718698ea8c95df4ba85584b
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-09-26 11:08:56 +0900

    ADD: 홈화면>학교상세>배치도
    기본 배치

--- unifesta/SchoolDetail.tsx
+++ unifesta/SchoolDetail.tsx
@@ -56,7 +56,38 @@ const FestivalInformation = () => (
   
 const FestivalLocation = () => (
     <View style={{ flex: 1, }}>
-        <Text  style={{fontSize:100}}>배치도</Text>
+        <View style={{flex:4,}}>
+            <Text>GoogleMap 지도 들어감</Text>
+        </View>
+        <View style={{flex:1,flexDirection:'row',}}>
+            <View style={{borderColor:'gray',borderWidth:1,margin:10,borderRadius:10,padding:1,}}>
+                <Text>DAY 1</Text>
+            </View>
+            <View style={{borderColor:'gray',borderWidth:1,margin:10,borderRadius:10,padding:1,}}>
+                <Text>DAY 2</Text>
+            </View>
+            <View style={{borderColor:'gray',borderWidth:1,margin:10,borderRadius:10,padding:1,}}>
+                <Text>DAY 3</Text>
+            </View>
+        </View>
+        <View style={{flex:8,}}>
+            <View>
+                <Text>1 부스이름</Text>
+                <Text>부스 설명 어쩌구 어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구</Text>
+            </View>
+            <View>
+                <Text>2 부스이름</Text>
+                <Text>부스 설명 어쩌구 어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구</Text>
+            </View>
+            <View>
+                <Text>3 부스이름</Text>
+                <Text>부스 설명 어쩌구 어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구</Text>
+            </View>
+            <View>
+                <Text>4 부스이름</Text>
+                <Text>부스 설명 어쩌구 어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구어쩌구</Text>
+            </View>
+        </View>
     </View>
 );
 const FestivalReview = () => (
